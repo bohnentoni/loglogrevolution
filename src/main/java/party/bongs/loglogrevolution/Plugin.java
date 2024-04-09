8ff06ef404bc599fe0e2c83fa6b051a2535b9118
Florian Hübner <fhuebner@posteo.de>
2024-04-09 02:15:36 +0200

    cleanup + start bfs refactor

--- src/main/java/party/bongs/loglogrevolution/Plugin.java
+++ src/main/java/party/bongs/loglogrevolution/Plugin.java
@@ -1,28 +1,81 @@
 package party.bongs.loglogrevolution;
 
+import com.google.common.collect.ImmutableMap;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.Nullable;
+import org.bukkit.util.Vector;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static org.bukkit.Material.*;
 
 public final class Plugin extends JavaPlugin implements Listener {
 
-    private static final Set<Material> logs = Set.of(OAK_LOG, ACACIA_LOG, BIRCH_LOG, CHERRY_LOG, DARK_OAK_LOG,
-            JUNGLE_LOG, MANGROVE_LOG, SPRUCE_LOG, CRIMSON_STEM, WARPED_STEM, MANGROVE_ROOTS);
+    private static final int limit = 420;
 
-    private static final Set<Material> leaves = Set.of(OAK_LEAVES, ACACIA_LEAVES, BIRCH_LEAVES, CHERRY_LEAVES,
-            DARK_OAK_LEAVES, JUNGLE_LEAVES, MANGROVE_LEAVES, SPRUCE_LEAVES, CRIMSON_HYPHAE, CRIMSON_NYLIUM,
-            WARPED_HYPHAE, WARPED_NYLIUM);
+    private static final Set<Vector> neighbors = Set.of(
+            new Vector( 0, 0, -1), // north
+            new Vector( 1, 0,  0), // east
+            new Vector( 0, 0,  1), // south
+            new Vector(-1, 0,  0), // west
+            new Vector( 1, 0, -1), // north-east
+            new Vector(-1, 0, -1), // north-west
+            new Vector( 1, 0,  1), // south-east
+            new Vector(-1, 0,  1)  // south-west
+    );
+
+    private static final Set<Material> logs = Set.of(
+            OAK_LOG,
+            SPRUCE_LOG,
+            BIRCH_LOG,
+            JUNGLE_LOG,
+            ACACIA_LOG,
+            DARK_OAK_LOG,
+            MANGROVE_LOG,
+            MANGROVE_ROOTS,
+            MUDDY_MANGROVE_ROOTS,
+            CHERRY_LOG,
+            CRIMSON_STEM,
+            WARPED_STEM
+    );
+
+    private static final Set<Material> axes = Set.of(
+            WOODEN_AXE,
+            STONE_AXE,
+            GOLDEN_AXE,
+            IRON_AXE,
+            DIAMOND_AXE,
+            NETHERITE_AXE,
+            LEGACY_WOOD_AXE,
+            LEGACY_STONE_AXE,
+            LEGACY_GOLD_AXE,
+            LEGACY_IRON_AXE,
+            LEGACY_DIAMOND_AXE
+    );
+
+    private static final Map<Material, Set<Material>> associated = ImmutableMap.<Material, Set<Material>>builder()
+            .put(OAK_LOG, Set.of(OAK_LEAVES))
+            .put(SPRUCE_LOG, Set.of(SPRUCE_LEAVES))
+            .put(BIRCH_LOG, Set.of(BIRCH_LEAVES))
+            .put(JUNGLE_LOG, Set.of(JUNGLE_LEAVES))
+            .put(ACACIA_LOG, Set.of(ACACIA_LEAVES))
+            .put(DARK_OAK_LOG, Set.of(DARK_OAK_LEAVES))
+            .put(MANGROVE_LOG, Set.of(MANGROVE_LEAVES, MANGROVE_ROOTS, MUDDY_MANGROVE_ROOTS))
+            .put(MANGROVE_ROOTS, Set.of(MANGROVE_LEAVES, MANGROVE_LOG, MUDDY_MANGROVE_ROOTS))
+            .put(MUDDY_MANGROVE_ROOTS, Set.of(MANGROVE_LEAVES, MANGROVE_LOG, MANGROVE_ROOTS))
+            .put(CHERRY_LOG, Set.of(CHERRY_LEAVES))
+            .put(CRIMSON_STEM, Set.of(NETHER_WART_BLOCK, SHROOMLIGHT))
+            .put(WARPED_STEM, Set.of(WARPED_WART_BLOCK, SHROOMLIGHT))
+            .build();
 
     @Override
     public void onEnable() {
@@ -32,53 +85,44 @@ public final class Plugin extends JavaPlugin implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onPlayerBlockBreakEvent(BlockBreakEvent ev) {
 
-        if (!isAxe(ev.getPlayer().getInventory().getItemInMainHand())) return;
+        // only logs
+        if (!logs.contains(ev.getBlock().getType())) return;
 
-        Set<Block> breakyLot = ConcurrentHashMap.newKeySet();
+        // only axes
+        if (!axes.contains(ev.getPlayer().getInventory().getItemInMainHand().getType())) return;
 
-        Block breaky = ev.getBlock();
-        breakyListyAddy(breakyLot, breaky);
+        Set<Block> breakies = new HashSet<>();
 
-        for (Block block : breakyLot) {
+        // trunk
+        breakies.addAll(bfs(ev.getBlock(), mat1 -> mat1.equals(ev.getBlock().getType())));
+
+        // leaves
+        breakies.addAll(bfs(ev.getBlock(), mat -> associated
+                .getOrDefault(ev.getBlock().getType(), Collections.emptySet())
+                .contains(mat)));
+
+        for (Block block : breakies) {
             block.breakNaturally();
+            // TODO: drop all loot in front of player
         }
     }
 
-    private void breakyListyAddy(Set<Block> breakies, Block breaky) {
-
-        // if block is already in our list, do nothing
-        if (breakies.contains(breaky)) return;
-
-        // if nor log or leaf, do nothing
-        if (!logs.contains(breaky.getType()) && !leaves.contains(breaky.getType())) return;
-
-        // put block in our list
-        breakies.add(breaky);
-
-        // for all neighbours
-        for (BlockFace face : BlockFace.values()) {
-            // call recursive
-            breakyListyAddy(breakies, breaky.getRelative(face));
-        }
+    private Set<Block> neighbors(Block center, Predicate<Material> filter) {
+        return neighbors.stream()
+                // grab all neighbors of the center block
+                .map(v -> center.getRelative(v.getBlockX(), v.getBlockY(), v.getBlockZ()))
+                // keep them if they match the material filter
+                .filter(block -> filter.test(block.getType()))
+                .collect(Collectors.toUnmodifiableSet());
     }
 
-    private boolean isAxe(@Nullable ItemStack stack) {
-        if (stack == null) return false;
-        return switch (stack.getType()) {
-            case WOODEN_AXE,
-                 STONE_AXE,
-                 GOLDEN_AXE,
-                 IRON_AXE,
-                 DIAMOND_AXE,
-                 NETHERITE_AXE,
-                 LEGACY_WOOD_AXE,
-                 LEGACY_STONE_AXE,
-                 LEGACY_GOLD_AXE,
-                 LEGACY_IRON_AXE,
-                 LEGACY_DIAMOND_AXE
-                    -> true;
-            default -> false;
-        };
-    }
+    private Set<Block> bfs(Block start, Predicate<Material> filter) {
+        Set<Block> blocks = new HashSet<>();
 
+        // TODO
+
+        if (blocks.size() >= limit) return blocks;
+
+        return blocks;
+    }
 }
